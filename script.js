341a0b4b02f5230c52d2f19271f664fd06927194
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-04 21:43:07 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ for (let i = 1; i < colorBlocks.length; i += 1) {
 }
 
 console.log(colors);
+const pixelBoard = document.getElementById('pixel-board');
+for (let i = 0; i < 25; i += 1) {
+  const pixel = document.createElement('span');
+  pixel.classList.add('pixel')
+  pixelBoard.appendChild(pixel);
+}
