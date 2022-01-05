a31e5010fbfcce38903efa9e5f09944bda29b506
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-05 00:17:14 -0300

    requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 let colors = ['black', 'red', 'green', 'blue'];
+const colorPalette = document.getElementById('color-palette')
+const pixelBoard = document.getElementById('pixel-board');
+
+// FUNÇÃO PARA LIMPAR TODOS OS PIXELS
+function doClearBoard() {
+  for (const pixel of pixelBoard.children) {
+    pixel.style.backgroundColor = 'white';
+  }
+}
 
 // FUNÇÃO PARA SELECIONAR E PINTAR
 function onPixelClick(event) {
   const target = event.target;
+  const selected = document.querySelector('span.color.selected');
   switch (target.classList[0]) {
     case 'color':
-      const selected = target.parentElement.querySelector('span.color.selected');
       if (target !== selected) {
         selected.classList.remove('selected');
         target.classList.add('selected');
       }
       break;
+      case 'pixel':
+        target.style.backgroundColor = selected.style.backgroundColor
+        break;
   }
 }
 
 // CRIAÇÃO DAS CORES DA PALETA DE CORES
-const colorPalette = document.getElementById('color-palette')
 for (let i = 0; i < colors.length; i += 1) {
   const colorPixel = document.createElement('span');
   colorPixel.classList.add('color')
@@ -26,9 +37,9 @@ for (let i = 0; i < colors.length; i += 1) {
 }
 
 // CRIAÇÃO DOS PIXELS DO PIXEL BOARD
-const pixelBoard = document.getElementById('pixel-board');
 for (let i = 0; i < 25; i += 1) {
   const pixel = document.createElement('span');
   pixel.classList.add('pixel')
+  pixel.addEventListener('click', onPixelClick)
   pixelBoard.appendChild(pixel);
 }
