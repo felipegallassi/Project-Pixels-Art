f73b0bf6349a4db4d3e154cfb711586496a4175c
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-04 23:00:42 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-let colors = ['red', 'green', 'blue'];
+let colors = ['black', 'red', 'green', 'blue'];
 
-const colorBlocks = document.getElementsByClassName('color');
-for (let i = 1; i < colorBlocks.length; i += 1) {
-  colorBlocks[i].style.backgroundColor = colors[i - 1];
+// FUNÇÃO PARA SELECIONAR E PINTAR
+function onPixelClick(event) {
+  const target = event.target;
+  switch (target.classList[0]) {
+    case 'color':
+      const selected = target.parentElement.querySelector('span.color.selected');
+      if (target !== selected) {
+        selected.classList.remove('selected');
+        target.classList.add('selected');
+      }
+      break;
+  }
 }
 
-console.log(colors);
+// CRIAÇÃO DAS CORES DA PALETA DE CORES
+const colorPalette = document.getElementById('color-palette')
+for (let i = 0; i < colors.length; i += 1) {
+  const colorPixel = document.createElement('span');
+  colorPixel.classList.add('color')
+  if (i === 0) { colorPixel.classList.add('selected'); }
+  colorPixel.style.backgroundColor = colors[i];
+  colorPalette.addEventListener('click', onPixelClick)
+  colorPalette.appendChild(colorPixel);
+}
+
+// CRIAÇÃO DOS PIXELS DO PIXEL BOARD
 const pixelBoard = document.getElementById('pixel-board');
 for (let i = 0; i < 25; i += 1) {
   const pixel = document.createElement('span');
